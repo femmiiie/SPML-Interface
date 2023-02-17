f83e50a7d27b1e89af356d86b7b4a650d134a459
femmiiie <sandromemail@yahoo.com>
2023-02-17 14:40:40 -0500

    began adding event listeners

--- scripts.js
+++ scripts.js
@@ -1,33 +1,49 @@
 
 function optionSelector() {
     let opt = document.getElementById("sheet-type").value;
+    console.log(opt);
     switch (opt) {
         case 'wind':
-            document.getElementById('grade').hidden = false;
-            document.getElementById('').hidden = false;
+            document.getElementById('grade').type = "number";
+            //document.getElementById('').hidden = false;
 
             break;
         case 'jazz':
-            document.getElementById('grade').hidden = false;
-            document.getElementById('').hidden = false;
+            document.getElementById('grade').type = "hidden";
+            //document.getElementById('').hidden = false;
 
             break;
         case 'misc':
-            document.getElementById('grade').hidden = true;
-            document.getElementById('').hidden = true;
+            document.getElementById('grade').type = "hidden";
+            //document.getElementById('').hidden = true;
 
             break;
     }
+    document.getElementById('sheet-type').onchange()
 }
 
+/*
+let tableType = document.getElementById('sheet-type');
+
+tableType.addEventListener('change', (val) => {
+    switch (val.target.value) {
+        case 'wind':
+            document.getElementById('grade').hidden = false;
+            break;
+        case 'jazz':
+            document.getElementById('grade').hidden = false;
+            break;
+        case 'misc':
+            document.getElementById('grade').hidden = true;
+            break;  
+    }
+})
+*/
 
 
 
 
 
-
-
-
-module.exports = {
+export {
     optionSelector
 }

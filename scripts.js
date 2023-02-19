c6894e3b17596479ab1ed0344ed487ed3ad41eb8
femmiiie <sandromemail@yahoo.com>
2023-02-19 18:48:29 -0500

    added proper reset and switch events

--- scripts.js
+++ scripts.js
@@ -1,49 +1,60 @@
 
+
+//Streamlines hiding and showing the input and its label
+function setHiddenWL(id) {
+    document.getElementById(id).setAttribute("hidden", "hidden");
+    document.getElementById(id + 'L').setAttribute("hidden", "hidden");
+}
+
+function removeHiddenWL(id) {
+    document.getElementById(id).removeAttribute("hidden");
+    document.getElementById(id + 'L').removeAttribute("hidden");
+}
+
+//Changes form display based on the value of sheettype select
 function optionSelector() {
-    let opt = document.getElementById("sheet-type").value;
+    let opt = document.getElementById("sheettype").value;
     console.log(opt);
+
     switch (opt) {
         case 'wind':
-            document.getElementById('grade').type = "number";
-            //document.getElementById('').hidden = false;
-
+            removeHiddenWL('grade');
+            setHiddenWL('type');
+            setHiddenWL('instrument');
+            setHiddenWL('amount');
+            removeHiddenWL('score');
+            removeHiddenWL('missing');
             break;
+
         case 'jazz':
-            document.getElementById('grade').type = "hidden";
-            //document.getElementById('').hidden = false;
-
+            setHiddenWL('grade');
+            setHiddenWL('type');
+            setHiddenWL('instrument');
+            setHiddenWL('amount');
+            removeHiddenWL('score');
+            removeHiddenWL('missing');
             break;
-        case 'misc':
-            document.getElementById('grade').type = "hidden";
-            //document.getElementById('').hidden = true;
 
+        case 'misc':
+            setHiddenWL('grade');
+            removeHiddenWL('type');
+            removeHiddenWL('instrument');
+            removeHiddenWL('amount');
+            setHiddenWL('score');
+            setHiddenWL('missing');
             break;
     }
-    document.getElementById('sheet-type').onchange()
+}
+//Resets options to wind band when pressing reset button
+function formReset() {
+    removeHiddenWL('grade');
+    setHiddenWL('type');
+    setHiddenWL('instrument');
+    setHiddenWL('amount');
+    removeHiddenWL('score');
+    removeHiddenWL('missing');
 }
 
-/*
-let tableType = document.getElementById('sheet-type');
-
-tableType.addEventListener('change', (val) => {
-    switch (val.target.value) {
-        case 'wind':
-            document.getElementById('grade').hidden = false;
-            break;
-        case 'jazz':
-            document.getElementById('grade').hidden = false;
-            break;
-        case 'misc':
-            document.getElementById('grade').hidden = true;
-            break;  
-    }
-})
-*/
-
-
-
-
-
-export {
-    optionSelector
-}
+//Event Listeners
+document.getElementById("sheettype").addEventListener("change", optionSelector)
+document.getElementById("searchform").addEventListener("reset", formReset)

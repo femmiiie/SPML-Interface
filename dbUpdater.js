ede57685eea1fed244bd057878bffe1177de8f0f
femmiiie <sandromemail@yahoo.com>
2023-03-01 19:53:04 -0500

    reformatted code

--- dbUpdater.js
+++ dbUpdater.js
@@ -1,11 +1,11 @@
-// Step 1: Connect to your SQL database
 const mysql = require('mysql');
+const XLSX = require('xlsx');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'username',
-  password: 'password',
-  database: 'database_name'
+  host: '',
+  user: '',
+  password: '',
+  database: ''
 });
 
 connection.connect(function(err) {
@@ -16,50 +16,51 @@ connection.connect(function(err) {
   console.log('Connected to database with ID ' + connection.threadId);
 });
 
-// Step 2: Read data from Excel file
-const XLSX = require('xlsx');
-
+//Read data from Excel file
 const workbook = XLSX.readFile('path/to/excel/file.xls');
-const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+const sheetNames = workbook.SheetNames;
 
-// Get the data from the Excel file
-const data = [];
-const range = XLSX.utils.decode_range(worksheet['!ref']);
+//Loop through all sheets
+sheetNames.forEach(sheetName => {
+  const worksheet = workbook.Sheets[sheetName];
+  const data = [];
+  const range = XLSX.utils.decode_range(worksheet['!ref']);
 
-// Get the last row that was updated
-const query = "SELECT MAX(id) as last_id FROM table_name";
-connection.query(query, function (error, results, fields) {
-  if (error) {
-    console.log("Error getting last ID: " + error);
-    return;
-  }
-
-  const last_id = results[0].last_id || 0;
-
-  for (let row = last_id + 1; row <= range.e.r; row++) {
-    const rowData = [];
-    for (let col = 0; col <= range.e.c; col++) {
-      const cellAddress = XLSX.utils.encode_cell({ r: row, c: col });
-      const cellValue = worksheet[cellAddress] ? worksheet[cellAddress].v : '';
-      rowData.push(cellValue);
+  // Get the last row that was updated
+  const query = `SELECT MAX(id) as last_id FROM ${sheetName}`;
+  connection.query(query, function (error, results, fields) {
+    if (error) {
+      console.log(`Error getting last ID for ${sheetName}: ${error}`);
+      return;
     }
-    data.push(rowData);
-  }
 
-  // Step 3: Update the SQL database with the new data from the Excel file
-  data.forEach(row => {
-    const sql = `INSERT INTO table_name (column1, column2) VALUES ('${row[0]}', '${row[1]}')`;
-    connection.query(sql, function (error, results, fields) {
-      if (error) {
-        console.log("Error updating record: " + error);
-        return;
+    const last_id = results[0].last_id || 0;
+
+    for (let row = last_id + 1; row <= range.e.r; row++) {
+      const rowData = [];
+      for (let col = 0; col <= range.e.c; col++) {
+        const cellAddress = XLSX.utils.encode_cell({ r: row, c: col });
+        const cellValue = worksheet[cellAddress] ? worksheet[cellAddress].v : '';
+        rowData.push(cellValue);
       }
-      console.log("Record updated: " + results.affectedRows);
+      data.push(rowData);
+    }
+
+    //Update the SQL database with the new data from the Excel file
+    data.forEach(row => {
+      const sql = `INSERT INTO ${sheetName} (column1, column2) VALUES ('${row[0]}', '${row[1]}')`;
+      connection.query(sql, function (error, results, fields) {
+        if (error) {
+          console.log(`Error updating record for ${sheetName}: ${error}`);
+          return;
+        }
+        console.log(`Record updated for ${sheetName}: ${results.affectedRows}`);
+      });
     });
   });
+});
 
-  // Step 4: Close the database connection
-  connection.end(function(err) {
-    console.log('Database connection closed.');
-  });
+//Close the database connection
+connection.end(function(err) {
+  console.log('Database connection closed.');
 });

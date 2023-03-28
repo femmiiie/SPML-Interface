3045f3c5771ea59d75f3c8922359bdba707c13b1
femmiiie <sandromemail@yahoo.com>
2023-03-27 22:52:28 -0400

    more robust query system

--- dbQuery.php
+++ dbQuery.php
@@ -1,35 +1,46 @@
 <?php
 
-// Check if the request method is POST
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// Define database credentials
+$host = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
 
-  // Filter the input data
-  $sheet = $_POST['sheettype'];
-  $title = filter_input(INPUT_POST, 'title');
-  $composer = filter_input(INPUT_POST, 'composer');
-  $arranger = filter_input(INPUT_POST, 'arranger');
-
-  $grade = $_POST['grade'];
-  $type = $_POST['type'];
-  $instrument = $_POST['instrument'];
-  $publisher = $_POST['publisher'];
-  $amount = filter_input(INPUT_POST, 'amount', FILTER_SANITIZE_NUMBER_INT);
-  $scoreYN = $_POST['score'];
-  $missingYN = $_POST['missing'];
-
-  // Create an SQL query using the filtered data
-  if ($sheet === 'Wind Ensemble Music'){
-    $sql = "SELECT * FROM Wind_Ensemble_Music
-    WHERE title LIKE '%$title%' AND composer LIKE '%$composer%' AND arranger LIKE '%$arranger%'
-    ORDER BY title;";
-  } elseif ($sheet === 'Jazz Music') {
-    $sql = "SELECT * FROM Jazz_Music
-    WHERE title LIKE '%$title%' AND composer LIKE '%$composer%' AND arranger LIKE '%$arranger%'
-    ORDER BY title;";
-  } else {
-    $sql = "SELECT * FROM Misc_Music
-    WHERE title LIKE '%$title%' AND composer LIKE '%$composer%' AND arranger LIKE '%$arranger%'
-    ORDER BY title;";
-  }
-
+// Connect to the database
+$conn = mysqli_connect($host, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
+// Get the form data
+$sheettype = $_POST["sheettype"];
+$title = $_POST["title"];
+$composer = $_POST["composer"];
+$arranger = $_POST["arranger"];
+$publisher = $_POST["publisher"];
+$score = isset($_POST["score"]) ? 1 : 0;
+$missing = isset($_POST["missing"]) ? 1 : 0;
+
+// Build the SQL query
+$sql = "SELECT * FROM music_library WHERE sheettype = '$sheettype'";
+if ($title !== "") {
+    $sql .= " AND title LIKE '%$title%'";
+}
+if ($composer !== "") {
+    $sql .= " AND composer LIKE '%$composer%'";
+}
+if ($arranger !== "") {
+    $sql .= " AND arranger LIKE '%$arranger%'";
+}
+if ($publisher !== "") {
+    $sql .= " AND publisher LIKE '%$publisher%'";
+}
+if ($score !== "") {
+    $sql .= " AND score = $score";
+}
+if ($missing !== "") {
+    $sql .= " AND missing = $missing";
+}
+
+// Execute the SQL query
+$result = mysqli_query($conn, $sql);
